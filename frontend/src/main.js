8f1b104d955ee2a10dc035e4562870bdcc293ad5
AlessandroDiNardo <alexdna003@gmail.com>
2023-01-13 15:23:09 +0100

    import file to vue, rename helloworld

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,6 @@
-/* Set up using Vue 2 */
-import Vue from 'vue'
-import App from './App'
+/* Set up using Vue 3 */
+import { createApp } from 'vue'
+import App from './App.vue'
 
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -14,14 +14,6 @@ import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 /* add icons to the library */
 library.add(faUserSecret)
 
-/* add font awesome icon component */
-Vue.component('font-awesome-icon', FontAwesomeIcon)
-
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
-new Vue({
-    el: '#app',
-    components: { App },
-    template: '<App/>'
-})
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
